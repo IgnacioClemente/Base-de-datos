60e24aa93950856bd3c2f6ec1e808d5491987ff4
hansow <nachoagus@live.com.ar>
2023-05-23 22:09:23 -0300

    pequeño cambio

--- empresa/empresa.DML.sql
+++ empresa/empresa.DML.sql
@@ -14,12 +14,9 @@ insert into empleados (nro_Legajo,nombre,apellido,codigo_departamento,tipo) valu
 insert into empleados (nro_Legajo,nombre,apellido,codigo_departamento,tipo) values(754884,'Mariano','No',787,'contratado');
 insert into empleados (nro_Legajo,nombre,apellido,codigo_departamento,tipo) values(968332,'Ernan','Plis',986,'contratado');
 
-
+-- - Ingreso registros de "efectivos y "contratados"
 insert into efectivos(nro_Legajo) select nro_Legajo from empleados where tipo = "efectivo"; -- me ahorro escribir los otros 2 insert, tipo seria otra variable mas de empleados
 insert into contratados(nro_Legajo) select nro_Legajo from empleados where tipo = "contratado"; -- me ahorro escribir los otros 2 insert, tipo seria otra variable mas de empleados
--- select * from efectivos
--- select * from contratados
--- update efectivos set salario = 120000, where(nro_legajo = 765635); -- ejemplo de update
 
 -- - Ingreso registros de "consultora"
 insert into consultora(cuil,nombre) values(19553252,'Suarez');
@@ -29,6 +26,7 @@ insert into consultora(cuil,nombre) values(89723525,'Riojas');
 UPDATE efectivos SET salario = 20000 WHERE (nro_Legajo = 346534);
 UPDATE efectivos SET salario = 130000 WHERE (nro_Legajo = 123145);
 UPDATE efectivos SET salario = 130000 WHERE (nro_Legajo = 765635);
+
 UPDATE contratados SET valorHoraComun = 800, valorHoraExtra = 300, horaComun = 8, horaExtra = 4, cuil_Consultora = 19553252 WHERE (nro_Legajo = 325325);
 UPDATE contratados SET valorHoraComun = 1500, valorHoraExtra = 543, horaComun = 8, horaExtra = 4, cuil_Consultora = 65853252 WHERE (nro_Legajo = 754884);
 UPDATE contratados SET valorHoraComun = 2424, valorHoraExtra = 633, horaComun = 8, horaExtra = 4, cuil_Consultora = 89723525 WHERE (nro_Legajo = 968332);
